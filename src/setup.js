d2076520fd200db54666b54f3314139615ac7474
Angélica Cavalheiro Rodrigues <angelicacavalheiro51@gmail.com>
2021-11-07 19:28:01 -0300

    update

---
+++ src/setup.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+
+let envFile =  '.env.test';
+
+//prod, dev, test são os nomes que os bancos tem
+
+if(process.env.NODE_ENV === "prod"){
+  envFile = '.env'
+}
+
+if(process.env.NODE_ENV === "dev"){
+  envFile = '.env-dev'
+}
+
+console.log('ENV' + envFile)
+
+dotenv.config({
+  path: envFile
+});

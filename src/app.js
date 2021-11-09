a41a633c747a76ecc083962ac145186d840519f5
Diego Mamede Nogueira <diegomamede@poli.ufrj.br>
2021-11-09 10:59:42 -0300

    SignIn e signOut feitos ja com testes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import singUp from './controllers/signUp/signUp.js';
+import signIn from './controllers/signIn/signIn.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/status', (req, res) => {
+  // Manda como resposta o texto 'Hello World'
+  res.send('Server online');
+});
+
+app.post('/sign-up', singUp);
+app.post('/sign-in', signIn);
 
 export default app;

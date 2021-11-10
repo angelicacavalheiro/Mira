ba2eb764cff6a45b3586c715157b1686047779d0
Diego Mamede Nogueira <diegomamede@poli.ufrj.br>
2021-11-10 20:46:14 -0300

    Cart e Checkout feitos, faltam testes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import singUp from './controllers/signUp/signUp.js';
 import signIn from './controllers/signIn/signIn.js';
 import getCartItems from './controllers/cartItems/getCartItems.js';
 import postCancelItem from './controllers/cartItems/postCancelItem.js';
+import getCheckOutList from './controllers/checkOut/getCheckOutList.js';
 
 const app = express();
 app.use(cors());
@@ -18,5 +19,6 @@ app.post('/sign-up', singUp);
 app.post('/sign-in', signIn);
 app.get('/cart', getCartItems);
 app.post('/cart', postCancelItem);
+app.get('/checkout', getCheckOutList);
 
 export default app;

4e17ec3e639d546d32164aad724183d4a9eed316
Angélica Cavalheiro Rodrigues <angelicacavalheiro51@gmail.com>
2021-11-29 12:03:37 -0300

    art route test with parameters, added

--- tests/galleryDetails.test.js
+++ tests/galleryDetails.test.js
@@ -11,12 +11,8 @@ afterAll(() => {
 describe('GET /gallery/:idGallery', () => {
   it('returns 200 for valid gallery_id', async () => {
     const gallery_id = await galleryIdFactory();
-
     const result = await supertest(app)
-
       .get(`/gallery/${gallery_id}`);
-
-    console.log(result.body[0]);
     const { status } = result;
 
     expect(status).toEqual(200);
@@ -36,16 +32,3 @@ describe('GET /gallery/:idGallery', () => {
   });
 });
 
-// expect(result.body[0]).toHaveProperty('idStock');
-// expect(result.body[0]).toHaveProperty('quantity');
-// expect(result.body[0]).toHaveProperty('price');
-// expect(result.body[0]).toHaveProperty('art_photo');
-// expect(result.body[0]).toHaveProperty('id');
-// expect(result.body[0]).toHaveProperty('galery_name');
-// expect(result.body[0]).toHaveProperty('phone_number');
-// expect(result.body[0]).toHaveProperty('description');
-// expect(result.body[0]).toHaveProperty('adress');
-// expect(result.body[0]).toHaveProperty('art_name');
-// expect(result.body[0]).toHaveProperty('artist_name');
-// expect(result.body[0]).toHaveProperty('artistDescription');
-// expect(result.body[0]).toHaveProperty('artistPhoto');
